55a4051ced216b4fe9c3b3b40c8ca395974fedc8
DarkXsOriginal <lev-zyryanov@mail.ru>
2025-07-02 23:28:41 +0800

    ...

--- CmpFiles/CmpFiles.cs
+++ CmpFiles/CmpFiles.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CmpFiles
 {
     public class Compressor
     {
-        public static void CompressImage(string SoucePath, string DestPath, int quality)
+        public static void CompressImage(string SourcePath, string DestPath, int quality)
         {
-            var fileName = Path.GetFileNameWithoutExtension(SoucePath);
+            var fileName = Path.GetFileNameWithoutExtension(SourcePath);
             string destFilePathWithoutExtension = DestPath + "\\" + fileName;
-            string originalExt = Path.GetExtension(SoucePath);
+            string originalExt = Path.GetExtension(SourcePath);
             string jpgExt = ".jpg";
 
             string originalDest = destFilePathWithoutExtension + originalExt;
@@ -23,15 +25,15 @@ namespace CmpFiles
 
             void justCopy()
             {
-                Console.WriteLine($"Не удаётся сжать \"{SoucePath}\", скопировано");
-                var ext = Path.GetExtension(SoucePath);
-                File.Copy(SoucePath, originalDest, true);
+                Console.WriteLine($"Не удаётся сжать \"{SourcePath}\", скопировано");
+                var ext = Path.GetExtension(SourcePath);
+                File.Copy(SourcePath, originalDest, true);
             }
 
             var tempJpg = Path.GetTempFileName() + jpgExt;
             try
             {
-                compressImage(SoucePath, tempJpg, 50);
+                compressImage(SourcePath, tempJpg, 50);
             }
             catch
             {
@@ -40,7 +42,8 @@ namespace CmpFiles
             }
 
             compressImage(tempJpg, jpgDest, quality);
-            var sourceInfo = new FileInfo(SoucePath);
+            ShrinkImage(tempJpg, jpgDest, quality);
+            var sourceInfo = new FileInfo(SourcePath);
             var resultInfo = new FileInfo(jpgDest);
             if (sourceInfo.Length < resultInfo.Length)
             {
@@ -51,9 +54,9 @@ namespace CmpFiles
 
 
         }
-        private static void compressImage(string SoucePath, string DestPath, int quality)
+        private static void compressImage(string SourcePath, string DestPath, int quality)
         {
-            using (Bitmap bmp1 = new Bitmap(SoucePath))
+            using (Bitmap bmp1 = new Bitmap(SourcePath))
             {
                 ImageCodecInfo pngEncoder = GetEncoder(ImageFormat.Jpeg);
                 System.Drawing.Imaging.Encoder QualityEncoder = System.Drawing.Imaging.Encoder.Quality;
@@ -76,5 +79,32 @@ namespace CmpFiles
             return null;
         }
 
+        public static void ShrinkImage(string SourcePath,string DestPath,int quality)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(SourcePath);
+            string jpgExt = ".jpg";
+            string destFilePathWithoutExtension = DestPath + "\\" + fileName;
+            string jpgDest = destFilePathWithoutExtension + jpgExt;
+            Bitmap bitmap;
+
+            using (Bitmap bmp = new Bitmap(SourcePath)){
+            double percentQuality = (double)quality / 100;
+            int newWidth = (int)(bmp.Width * percentQuality);
+            int newHeight = (int)(bmp.Height * percentQuality);
+            bitmap = ResizeBitmap(bmp, newWidth, newHeight);
+            }
+            bitmap.Save(SourcePath, ImageFormat.Jpeg);
+            Console.WriteLine("Разрешение изображения изменено!!");
+        }
+
+        public static Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
+        {
+            Bitmap bitmap = new Bitmap(width, height);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.DrawImage(bmp, 0, 0, width, height);
+            }
+            return bitmap;
+        }
     }
 }
